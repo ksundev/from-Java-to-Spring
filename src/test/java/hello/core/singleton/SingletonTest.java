bbf6567a8d8b96a9850d1429711ca27c8e9a963f
ksundev <kshk08@hanyang.ac.kr>
2025-03-19 15:55:05 +0900

    feat: 싱글톤 패턴을 적용한 SingletonService 클래스 추가 및 테스트 케이스 작성

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -24,4 +24,18 @@ public class SingletonTest {
     // memberService1 != memberService2
     Assertions.assertThat(memberService1).isNotSameAs(memberService2);
   }
+
+  @Test
+  @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+  void singleTonServiceTest() {
+    SingletonService singletonService1 = SingletonService.getInstance();
+    SingletonService singletonService2 = SingletonService.getInstance();
+
+    // 참조값이 같은 것을 확인
+    System.out.println("singletonService1 = " + singletonService1);
+    System.out.println("singletonService2 = " + singletonService2);
+    Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    // same == (참조 비교)
+    // equal == (자바의 equals 비교)
+  }
 }
