54918ea9043e3f7279c56899075b71e331c5d55e
ksundev <kshk08@hanyang.ac.kr>
2025-03-25 17:31:39 +0900

    feat: 스프링 컴포넌트 스캔을 위한 AutoAppConfig 클래스 추가 및 여러 서비스 클래스에 @Component 어노테이션 추가

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+    // 실습에서 만들었던 Config들은 스캔 대상에서 제외.
+    // 보통 설정정보(Config)를 스캔 대상에서 제외하지는 않지만, 기존의 예제 코드를 최대한 살리기 위해 이 방법을 선택.
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,17 +2,19 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
-public class RateDiscountPolicy implements DiscountPolicy{
+@Component
+public class RateDiscountPolicy implements DiscountPolicy {
 
-    private int discountRate = 10;
+  private int discountRate = 10;
 
-    @Override
-    public int discount(Member member, int price) {
-        if (member.getGrade() == Grade.VIP) {
-            return price * discountRate / 100;
-        } else {
-            return 0;
-        }
+  @Override
+  public int discount(Member member, int price) {
+    if (member.getGrade() == Grade.VIP) {
+      return price * discountRate / 100;
+    } else {
+      return 0;
     }
+  }
 }
