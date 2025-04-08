4b9f50fcddc2058c054193a2355c035e11f0c652
ksundev <kshk08@hanyang.ac.kr>
2025-04-09 01:58:30 +0900

    feat: OrderServiceImpl에 @Autowired 및 커스텀 어노테이션 적용

--- README.md
+++ README.md
@@ -6,9 +6,13 @@
 - alt(option) + enter : 에러 해결 방법 제시
 - command + shift + enter : 코드 자동 완성
 
-## lombok 설정법
+### lombok 설정법
 
 - build.gradle 에 설정 및 라이브러리 추가 하기
 - settings 에서 annotation processors 제일 위에 Enable로 체크 해주기
 - @Getter @Setter @ToString @RequiredArgsConstructor 등 사용 가능
 - 컴파일 시 코드가 자동생성되는 원리로 코드 작성을 깔끔하고 편리하게 가능
+
+### 커스텀 어노테이션?
+
+- `@Primary` 대신에 `@Qualifier`를 사용한다면, 커스텀 어노테이션을 적용하는 것도 방법이다.
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,20 +1,26 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-  // 테스트 용도
   @Getter
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
+
+  @Autowired
+  public OrderServiceImpl(MemberRepository memberRepository,
+      @MainDiscountPolicy DiscountPolicy discountPolicy) {
+    this.memberRepository = memberRepository;
+    this.discountPolicy = discountPolicy;
+  }
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -23,5 +29,4 @@ public class OrderServiceImpl implements OrderService {
 
     return new Order(memberId, itemName, itemPrice, discountPrice);
   }
-
 }
