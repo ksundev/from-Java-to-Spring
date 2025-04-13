34281a8554530391e81ba905d150dd5f981e284f
ksundev <kshk08@hanyang.ac.kr>
2025-04-13 18:11:48 +0900

    refactor: PrototypeScope 클래스를 PrototypeBean으로 이름 변경 및 ObjectProvider를 사용하여 프로토타입 빈 주입 방식 개선

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -15,12 +16,12 @@ public class SingletonWithPrototypeTest1 {
   @Test
   void prototypeFind() {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
-        PrototypeScope.class);
-    PrototypeScope prototypeBean1 = ac.getBean(PrototypeScope.class);
+        PrototypeBean.class);
+    PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
     prototypeBean1.addCount();
     assertThat(prototypeBean1.getCount()).isEqualTo(1);
 
-    PrototypeScope prototypeBean2 = ac.getBean(PrototypeScope.class);
+    PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
     prototypeBean2.addCount();
     assertThat(prototypeBean2.getCount()).isEqualTo(1);
   }
@@ -28,28 +29,26 @@ public class SingletonWithPrototypeTest1 {
   @Test
   void singletonClientUsePrototype() {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
-        ClientBean.class, PrototypeScope.class);
+        ClientBean.class, PrototypeBean.class);
 
     ClientBean clientBean1 = ac.getBean(ClientBean.class);
     ClientBean clientBean2 = ac.getBean(ClientBean.class);
     int count1 = clientBean1.logic();
     assertThat(count1).isEqualTo(1);
     int count2 = clientBean2.logic();
-    assertThat(count2).isEqualTo(2);
+    assertThat(count2).isEqualTo(1);
   }
+
 
   @Component
   @Scope("singleton")
   static class ClientBean {
 
-    private final PrototypeScope prototypeBean; // 생성시점에 주입
-
     @Autowired
-    public ClientBean(PrototypeScope prototypeBean) {
-      this.prototypeBean = prototypeBean;
-    }
+    private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
     public int logic() {
+      PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
       prototypeBean.addCount();
       return prototypeBean.getCount();
     }
@@ -57,7 +56,7 @@ public class SingletonWithPrototypeTest1 {
 
   @Component
   @Scope("prototype")
-  static class PrototypeScope {
+  static class PrototypeBean {
 
     private int count = 0;
 
